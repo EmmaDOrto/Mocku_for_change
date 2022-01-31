864c64352b11b3c225f735fa068ea06b6f4ecfc0
Emma DOrto <emma.dorto@studio.unibo.it>
2022-01-31 11:06:17 +0100

    Created correlation function.

--- analysis.py
+++ analysis.py
@@ -8,38 +8,45 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import functions as f
 
-df= f.import_and_clean("thesis_data", "temi")
+df = f.import_and_copy("thesis_data", "temi")
+#df = df.fillna('Assente', inplace=True)
 
-cond_names = ["cond_" + str(i) for i in range(1,5)]
-conditions = pd.Series(["Risultato =='Presente'", 
-                        "Risultato =='Presente'| Risultato =='Debole'", 
-                        "(Risultato =='Presente' | Risultato =='Debole') & (Percezione == 'Presente'| Percezione =='Debole')", 
-                        "Intenzione =='Presente' & Risultato =='Presente' & Percezione == 'Presente'"], 
-                  index = cond_names, name = 'Condizione')
 
+conditions = {"1": { "description": "Topic present in intention", "code": "Intenzione =='Presente'"}, 
+              "2": { "description": "Topic present or slightly present in intention", "code": "Intenzione =='Presente'| Intenzione =='Debole'"},
+              "3": { "description": "Topic present in result", "code": "Risultato =='Presente'"},      
+              "4": { "description": "Topic present or slightly present in result", "code": "Risultato =='Presente'| Risultato =='Debole'"},      
+              "5": { "description": "Topic present in perception", "code": "Percezione =='Presente'"},
+              "6": { "description": "Topic present or slightly present in perception", "code": "Percezione =='Presente'| Percezione =='Debole'"},
+             }
 
 index_name = ['G' + str(i) for i in range(1,8)]
 score = pd.Series([1.97, 1.54, 2.93, 4.60, 4.61, 3.46, 4.53], 
                   index = index_name, name = 'Punteggio')
 
 
-for cond in conditions:
-    df_filtered = df.query(cond)
+for key in conditions:
+    
+    df_filtered = df.query(conditions[key]["code"])
     df_count = df_filtered.groupby(['Gruppo']).count().drop(['Intenzione','Risultato','Percezione'], 
                                                             axis=1).rename(columns={'Tema':'N_temi'})
     df_cond = df_filtered[['Gruppo', 'Tema']].groupby(['Gruppo']).aggregate({'Tema':'/'.join})
     df_join = pd.concat([df_cond, df_count, score], axis=1, join='inner').reset_index().sort_values('Punteggio')
     df_join.rename(columns={'index':'Gruppo', 'Tema': 'Tipo'}, inplace=True) 
     
+    print(conditions[key]["description"])
     print(df_join) 
 
 
     x = df_join.loc[:,"Punteggio"]
     y = df_join.loc[:,"N_temi"]
-    plt.scatter(x,y, s=100, c='green')
+    plt.scatter(x,y, s=100, c='blue')
+    plt.xlabel('Punteggio di genere',fontsize=12)
+    plt.ylabel('Conteggio temi',fontsize=12)
     plt.show()
    
-    
+    R = f.calculate_correlation(x,y)
+    print("Correlazione = ",R)
    
 
     
--- functions.py
+++ functions.py
@@ -5,11 +5,11 @@ Created on Sat Jan 29 12:40:05 2022
 @author: Emma
 """
 import pandas as pd
+import numpy as np
 
-def import_and_clean(filename, sheet):
+def import_and_copy(filename, sheet):
     original_df = pd.read_excel(filename + ".xlsx", sheet_name=sheet)
     df = original_df.copy()
-    df.fillna('Assente', inplace=True)
     return(df)
 
 
@@ -18,13 +18,18 @@ def filter_and_count(df, condition):
     return(df_join)
 
 
-def calculate_genre_score():
-    
-    return(score)
+def calculate_genre_score(element_use, element_coherence, pub_score, jury_score):
 
-def calculate_correlation():
-    
-    return(correlation)
+    return(genre_score)
+
+def calculate_correlation(x,y):
+    N = len(x)
+    x = np.asarray(x)
+    y = np.asarray(y)
+    num = N*np.sum(x*y) - (np.sum(x) * np.sum(y))
+    den = np.sqrt((N*np.sum(x**2) - np.sum(x)**2) * (N * np.sum(y**2) - np.sum(y)**2))
+    R = num/den
+    return(R)
 
 def graph_plot():
     
