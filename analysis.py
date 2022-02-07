18abb4e8e5c41a32031bb94848b6133213b2c94b
Emma DOrto <emma.dorto@studio.unibo.it>
2022-02-07 15:25:54 +0100

    Updated analysis file

--- analysis.py
+++ analysis.py
@@ -19,18 +19,21 @@ conditions = {"Topic present in intention": "Intention =='Present'",
               "Topic present or slightly present in result": "Result =='Present'| Result =='Slightly'",      
               "Topic present in perception": "Perception =='Present'",
               "Topic present or slightly present in perception": "Perception =='Present'| Perception =='Slightly'",
+              "Topic present in intention or result or perception": "Intention =='Present'| Result =='Present' | Perception =='Present'",
+              "Topic present or slighlty present in intention or result or perception": "Intention =='Present'| Intention =='Slightly'| Result =='Present' | Result =='Slightly' | Perception =='Present' | Perception =='Slightly'"
+           
               }
 
 groups = f.extract_group_names(df)
 
 genre_scores = []
-for group in groups: 
+for group in groups:
     group_score = f.calculate_group_score(df_elements, df_score, group)
     genre_scores.append(group_score)
-    
 score = pd.Series(genre_scores, index = groups, name = "Score")
 
 correlations = []
+correlations_squared = []
 for key in conditions:
     
     df_filtered = df.query(conditions[key]) 
@@ -38,17 +41,17 @@ for key in conditions:
     df_counted = f.count_fulfillments(df_filtered)
     
     df_final = pd.concat([df_counted, score], axis=1, join='outer')
-    df_final.fillna(0, inplace=True)
+   
     
-    df_final.name = (key)
+    df_final_name = (key)
     
-    print(df_final.name)
-    print(df_final) 
+    print(df_final_name)
+    print(df_final)
 
     x = df_final.Score
     y = df_final.Count
     plt.scatter(x,y, s=100, c='blue')
-    plt.title(df_final.name)
+    plt.title(df_final_name)
     plt.xlabel('Genre score',fontsize=12)
     plt.ylabel('Topics count',fontsize=12)
     for i in range(df_final.shape[0]): 
@@ -56,11 +59,16 @@ for key in conditions:
     plt.show()
     
     R = f.calculate_correlation(x,y)
-    print("Correlation = ", R)
+    print("R = ", round(R,2))
+    print("R^2 = ", round(R**2, 2))
     print("----------------------------------------------------------------")
-    correlations.append(R)
+    correlations.append(round(R,2))
+    correlations_squared.append(round(R**2, 2))
 
 conditions_names = list(conditions.keys()) 
-condition_score_correlations = pd.Series(correlations, index = conditions_names, name= "Correlation with Genre score")
+condition_score_correlations = pd.DataFrame(list(zip(correlations, correlations_squared)), index = conditions_names,
+               columns =['Pearson coefficient', 'R^2']).sort_values("R^2", ascending=False)
+                                            
 
 print(condition_score_correlations)
+#condition_score_correlations.to_excel("D:\TESI\correlation.xlsx")
